28a2baa0e37d8de0354a2913e215265b34e85400
a021704062c <a021704062c@S09-PC13>
2017-11-13 09:02:34 +0100

    Connection updated

--- proyectoServidor-master/src/main/java/Helper/ConnectionClassHelper.java
+++ proyectoServidor-master/src/main/java/Helper/ConnectionClassHelper.java
@@ -40,7 +40,7 @@ public class ConnectionClassHelper {
     }
 
     public static String getDatabasePassword() {
-        return "admin";
+        return "bitnami";
     }
 
     public static String getDatabasePort() {
@@ -48,7 +48,7 @@ public class ConnectionClassHelper {
     }
 
     public static String getDatabaseIP() {
-        return "192.168.122.26";
+        return "127.0.0.1";
     }
 
     public static String getConnectionChain() {
